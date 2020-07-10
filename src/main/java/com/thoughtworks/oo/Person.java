c37b74880dd0d4032965f6e323e7ff49574c9737
zhaohuanhuan <1297972106@qq.com>
2020-07-10 16:30:06 +0800

    完成需求一功能

--- src/main/java/com/thoughtworks/oo/Person.java
+++ src/main/java/com/thoughtworks/oo/Person.java
@@ -1,4 +1,13 @@
 package com.thoughtworks.oo;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public String introduce(String name, int age){
+        String returnStr = "My name is " + name + ". I am " + age + " years old.";
+        return returnStr;
+
+    }
 }
+
---
+++ src/test/java/com/thoughtworks/oo/PersonTest.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.oo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PersonTest {
+    @Test
+    public void should_return_relevant_mess_when_introduce(){
+        Person tom = new Person();
+        String mess = tom.introduce("TomFord", 18);
+        assertEquals(mess, "My name is TomFord. I am 18 years old.");
+    }
+}
